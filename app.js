d213169dd8631c4588fc884b83aa5108a64953dc
lukas <luki.hertel@gmail.com>
2021-11-21 17:43:54 +0100

    User can save iCal string

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function (req, res) {
     res.render('index');
 });
+
+app.post('/', function(req, res) {
+    var cal = req.body.ical;
+
+    fs.writeFile('config.dis', cal, function(err) {
+        if(err){
+            console.log("Failed to write to file: " + err);
+        }
+    })
+    return res.redirect('/');
+})
 
 app.set('view engine', 'ejs');
 
