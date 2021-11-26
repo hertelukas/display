65603f7751c4874fe1966f4ce0aefe179da476df
lukas <luki.hertel@gmail.com>
2021-11-26 18:59:58 +0100

    Fixed typo

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ try:
 
 
     # Info
-    width, height = font14.getsize(datetime.now().strftime("Last update: %H:%M"))
+    width, height = font14.getsize(datetime.datetime.now().strftime("Last update: %H:%M"))
     draw.text((800 - width, 480 - height), datetime.datetime.now().strftime("Last update: %H:%M"), font=font14, fill=0)
 
     epd.display(epd.getbuffer(image))
