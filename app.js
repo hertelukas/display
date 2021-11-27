a87051bc434623fc93e6ba7c213b1febf33a5dd9
lukas <luki.hertel@gmail.com>
2021-11-27 13:58:06 +0100

    Its now possible to setup a crontab

--- app.js
+++ app.js
@@ -10,11 +10,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', function (req, res) {
     fs.readFile('config.dis', 'utf8', function(err, data) {
         if(err){
-            //TODO handle error
-            return;
+            console.log("Failed to open config.dis: " + err.message)
         }
-        return res.render('index', {config: JSON.parse(data)});
-
+        fs.readFile('cron.dis', 'utf8', function(err, cron){
+            if(err){
+                console.log("Failed to open cron.dis: " + err.message)
+            }
+            return res.render('index', {config: JSON.parse(data), cron: cron});
+        });
     });
 });
 
@@ -35,13 +38,42 @@ app.post('/update', function(req, res) {
     console.log("Trying to update the screen...");
     exec('python3 main.py', (err, stdout, stderr) => {
         if(err){
-            console.log("Failed: " + err.message)
+            console.log("Failed: " + err.message);
         }else {
             console.log("Stdout: " + stdout);
             console.log("Stderr: " + stderr);
         }
     });
     return res.redirect('/');
+});
+
+app.post('/cron', function(req, res) {
+    console.log("Updating crontab...");
+    console.log("Writing to file...");
+    fs.writeFile('cron.dis', req.body['cron'], function(err) {
+        if(err){
+            console.log("Failed to write file: " + err);
+        } else{
+            //Write correct crontab file
+            fs.writeFile('crontab.dis', req.body['cron'] + " python3 main.py >> display.log", function(err){ 
+                if(err){
+                    console.log("Failed to write file: " + err);
+                } else{
+                    console.log("Saved new crontab to file successfully. Updating crontab...");
+                    exec('crontab crontab.dis', (err, stdout, stderr) => {
+                        if(err){
+                            console.log("Failed: " + err.message);
+                        }else{
+                            console.log("Command ran successfully.");
+                            console.log("Stdout: " + stdout);
+                            console.log("Stderr: " + stderr);
+                            return res.redirect('/');
+                        }
+                    });
+                }
+            });
+        }
+    });
 });
 
 app.post('/', function(req, res) {
