2fdb5820c4a24192a2231d208efc668574a6c470
lukas <luki.hertel@gmail.com>
2021-12-13 11:21:24 +0100

    Setting port to 80

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ app.post('/', function(req, res) {
 });
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port, function() {
     console.log("Server started. Listening on port " + port);
 })
