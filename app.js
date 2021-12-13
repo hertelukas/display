061c807bdae2db4f848674e21aaf4540570056a9
lukas <luki.hertel@gmail.com>
2021-12-13 11:31:30 +0100

    Setting port to 3000

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ app.post('/', function(req, res) {
 });
 
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 app.listen(port, function() {
     console.log("Server started. Listening on port " + port);
 })
